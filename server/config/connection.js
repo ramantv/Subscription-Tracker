fa268eaaf1d72f014e1f20aaf804988745338211
Naomi-Eckhoff <nsuibhne@gmail.com>
2022-02-24 13:54:02 -0600

    skeleton setup

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/subtrack-db', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/subtrack-db', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
