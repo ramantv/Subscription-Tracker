cfacd3350823259d92300115e6f1071513e0d311
Naomi-Eckhoff <nsuibhne@gmail.com>
2022-02-24 14:19:52 -0600

    copied more code from the template

--- client/src/components/CollapsibleDrawer/index.js
+++ client/src/components/CollapsibleDrawer/index.js
@@ -10,6 +10,28 @@ import Skeleton from '@mui/material/Skeleton';
 import Typography from '@mui/material/Typography';
 import SwipeableDrawer from '@mui/material/SwipeableDrawer';
 
+const drawerBleeding = 56;
+
+const Root = styled('div')(({ theme }) => ({
+  height: '100%',
+  backgroundColor:
+    theme.palette.mode === 'light' ? grey[100] : theme.palette.background.default,
+}));
+
+const StyledBox = styled(Box)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'light' ? '#fff' : grey[800],
+}));
+
+const Puller = styled(Box)(({ theme }) => ({
+  width: 30,
+  height: 6,
+  backgroundColor: theme.palette.mode === 'light' ? grey[300] : grey[900],
+  borderRadius: 3,
+  position: 'absolute',
+  top: 8,
+  left: 'calc(50% - 15px)',
+}));
+
 function CollapsibleDrawer(props) {
   const { window } = props;
   const [open, setOpen] = React.useState(false);
@@ -76,7 +98,7 @@ function CollapsibleDrawer(props) {
   );
 }
 
-SwipeableEdgeDrawer.propTypes = {
+CollapsibleDrawer.propTypes = {
   /**
    * Injected by the documentation to work in an iframe.
    * You won't need it on your project.
