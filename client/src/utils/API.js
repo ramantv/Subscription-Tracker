00a6058cb0eb658654fd7c2e5247e4e94b25842e
Bill Morris <bilcon@swbell.net>
2022-02-26 13:47:05 -0600

    get tv

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,21 +1,24 @@
 const API_KEY = TMDB_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
-const API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
+const GET_MOVIE_URL =
+  BASE_URL +
+  "/search/movie?&page=1&include_adult=false&api_key=" +
+  API_KEY +
+  "&query=";
+const GET_TV_URL =
+  BASE_URL +
+  "/search/tv?&page=1&include_adult=false&api_key=" +
+  API_KEY +
+  "&query=";
 
-getMovies(API_URL);
-
-function getMovies(url) {
-  fetch(url)
+export function getMovies(query) {
+  fetch(GET_MOVIE_URL + query)
     .then((res) => res.json())
-    .then((data) => {
-      showMovies(data.results);
-    });
+    .then((data) => data);
 }
 
-function showMovies(data) {
-  data.forEach(movie => {
-    const movieEl = document.createElement('div');
-    movieEl.classList.add('movie');
-    movieEl.innerHTML = 
-  })
+export function getTV(query) {
+  fetch(GET_TV_URL + query)
+    .then((res) => res.json())
+    .then((data) => data);
 }
