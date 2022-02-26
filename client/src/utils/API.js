d984784ff634a5cce227af7a91e3c72b0297212a
Bill Morris <bilcon@swbell.net>
2022-02-26 14:17:05 -0600

    get provider movie tv

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -10,15 +10,32 @@ const GET_TV_URL =
   "/search/tv?&page=1&include_adult=false&api_key=" +
   API_KEY +
   "&query=";
+const GET_MOVIE_PROVIDER1 = BASE_URL + "/movie/";
+const GET_MOVIE_PROVIDER2 =
+  "/watch/providers?language=en-US&api_key=" + API_KEY;
+const GET_TV_PROVIDER1 = BASE_URL + "/tv/";
+const GET_TV_PROVIDER2 = "/watch/providers?language=en-US&api_key=" + API_KEY;
 
 export function getMovies(query) {
-  fetch(GET_MOVIE_URL + query)
+  return fetch(GET_MOVIE_URL + query)
     .then((res) => res.json())
     .then((data) => data);
 }
 
 export function getTV(query) {
-  fetch(GET_TV_URL + query)
+  return fetch(GET_TV_URL + query)
+    .then((res) => res.json())
+    .then((data) => data);
+}
+
+export function getMovieProvider(id) {
+  return fetch(GET_MOVIE_PROVIDER1 + id + GET_MOVIE_PROVIDER2)
+    .then((res) => res.json())
+    .then((data) => data);
+}
+
+export function getTVProvider(id) {
+  return fetch(GET_TV_PROVIDER1 + id + GET_TV_PROVIDER2)
     .then((res) => res.json())
     .then((data) => data);
 }
