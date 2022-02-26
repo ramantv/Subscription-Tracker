e855d90e34117ed0977563e0dcde29882bb81f97
Bill Morris <bilcon@swbell.net>
2022-02-26 13:30:46 -0600

    get movies

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,10 +1,21 @@
-// route to get logged in user's info (needs the token)
-export const getMe = (token) => {
-  return fetch("/api/users/me", {
-    headers: {
-      "Content-Type": "application/json",
-      authorization: `Bearer ${token}`,
-    },
-  });
-};
+const API_KEY = TMDB_API_KEY;
+const BASE_URL = "https://api.themoviedb.org/3";
+const API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
 
+getMovies(API_URL);
+
+function getMovies(url) {
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      showMovies(data.results);
+    });
+}
+
+function showMovies(data) {
+  data.forEach(movie => {
+    const movieEl = document.createElement('div');
+    movieEl.classList.add('movie');
+    movieEl.innerHTML = 
+  })
+}
