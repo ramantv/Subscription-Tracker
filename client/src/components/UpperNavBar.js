01fbf04615852b155e93ac31af8dfe64cfa8142a
Naomi-Eckhoff <nsuibhne@gmail.com>
2022-02-28 08:25:23 -0600

    setup text on navbar

--- client/src/components/UpperNavBar.js
+++ client/src/components/UpperNavBar.js
@@ -4,18 +4,17 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
 import LogoutIcon from '@mui/icons-material/Logout';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import MovieIcon from '@mui/icons-material/Movie';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { Redirect } from "react-router";
 import Auth from "../utils/auth";
 
-const pages = ['Dashboard', 'Manage Subscriptions', 'Watch List'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 const logout = () => {
   if (Auth.loggedIn()) {
@@ -25,23 +24,6 @@ const logout = () => {
 };
 
 const ResponsiveAppBar = () => {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
 
   return (
     <AppBar position="absolute">
@@ -57,42 +39,6 @@ const ResponsiveAppBar = () => {
             Dashboard
           </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
           <Typography
             variant="h6"
             noWrap
@@ -102,15 +48,32 @@ const ResponsiveAppBar = () => {
             LOGO
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page}
-              </Button>
-            ))}
+
+            <Button
+              sx={{ my: 2, color: 'white', display: 'block' }}
+            >
+              <DashboardIcon />
+              Dashboard
+            </Button>
+            <Button
+              sx={{ my: 2, color: 'white', display: 'block' }}
+            >
+              <SubscriptionsIcon />
+              Manage Subscriptions
+            </Button>
+            <Button
+              sx={{ my: 2, color: 'white', display: 'block' }}
+            >
+              <MovieIcon />
+              Watch List
+            </Button>
+            <Button
+              sx={{ my: 2, color: 'white', display: 'block' }}
+            >
+              <AccountCircleIcon />
+              User Profile
+            </Button>
+
           </Box>
 
           <Box sx={{ flexGrow: 0 }}>
