6ad818a5d5a4c5d5dde2ac7e135e1ece3f5b62d2
Naomi-Eckhoff <nsuibhne@gmail.com>
2022-02-28 08:34:22 -0600

    cleaned up more of the code

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,25 +1,15 @@
 import React from "react";
-import { styled, createTheme, ThemeProvider } from "@mui/material/styles";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import MuiDrawer from "@mui/material/Drawer";
 import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import List from "@mui/material/List";
 import Typography from "@mui/material/Typography";
-import Divider from "@mui/material/Divider";
-import IconButton from "@mui/material/IconButton";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import Link from "@mui/material/Link";
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 
 import { useQuery } from "@apollo/client";
 
-import {
-  mainMenuItems,
-  secondaryMenuItems,
-} from "../components/LeftNavMenuItems";
 import Subscriptions from "../components/Subscriptions";
 //import Chart from "./Chart";
 //import Deposits from "./Deposits";
