1fc8fa65482bb4966d56b4bfa44742661eaba86e
Naomi-Eckhoff <nsuibhne@gmail.com>
2022-02-28 20:53:28 -0600

    removed unused import

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -10,7 +10,6 @@ import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 import backgroundImg from "../assets/landing2.jpg";
